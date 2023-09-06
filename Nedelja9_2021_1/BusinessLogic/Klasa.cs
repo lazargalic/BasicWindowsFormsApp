c5d0f8adcd80c5a12a48c9a30a0da649235d000b
Lazar <lazar.galic.business@gmail.com>
2023-09-06 18:30:05 +0200

    Add project files.

---
+++ Nedelja9_2021_1/BusinessLogic/Klasa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nedelja9_2021_1.BusinessLogic
+{
+    public class Klasa
+    {
+        private Klasa()
+        {
+
+        }
+        private static Klasa objekat;
+        public static Klasa Singleton
+        {
+            get
+            {
+                if (objekat == null)
+                {
+                    objekat = new Klasa();
+                }
+                return objekat;
+            }
+        }
+    }
+
+    public class Main 
+    {
+        public void MainMethod()
+        {
+            var instanca = Klasa.Singleton;
+            var instanca2 = Klasa.Singleton;
+        }
+    }
+}
